693020307d5502e0873ad6ffca3275c1317da9d1
mahdirahmani1376 <rahmanimahdi20@yahoo.com>
2023-09-29 17:44:35 +0330

    commit

--- aihrCrawler/aihrCrawler/spiders/aihr.py
+++ aihrCrawler/aihrCrawler/spiders/aihr.py
@@ -1,8 +1,14 @@
+import os.path
+
 import scrapy
 # from scrapy.cmdline import execute
 # execute("scrapy crawl aihr".split())
 from scrapy.crawler import CrawlerProcess
 from scrapy.selector import Selector
+from scrapy import cmdline
+
+# if not os.path.exists('/files'):
+#     os.makedirs('/files')
 
 
 class AihrSpider(scrapy.Spider):
@@ -21,13 +27,13 @@ class AihrSpider(scrapy.Spider):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
         'x-requested-with': 'XMLHttpRequest',
     }
-    allowed_domains = ["https://www.aihr.com"]
+    # allowed_domains = ["https://www.aihr.com"]
     start_url = """https://www.aihr.com/wp-admin/admin-ajax.php?action=alm_get_posts&query_type=standard&id=&post_id=114
     &slug=articles&canonical_url=https:%2F%2Fwww.aihr.com%2Fblog%2Fcategory%2Farticles%2F&posts_per_page=6
     &offset=0&post_type=post&repeater=template_4&seo_start_page=1&category=articles&order=DESC&orderby=date"""
 
     def start_requests(self):
-        yield scrapy.Request(url=self.start_url+"&page=0",callback=self.parse,headers=self.headers)
+        yield scrapy.Request(url=self.start_url + "&page=0", callback=self.parse, headers=self.headers)
 
     def parse(self, response):
         html = response.json()['html']
@@ -35,14 +41,38 @@ class AihrSpider(scrapy.Spider):
         selector = Selector(text=html)
         urlsList = list(set(selector.xpath('//a/@href').getall()))
         for i in urlsList:
-            yield response.follow(i,callback=self.ajaxRequest)
+            yield response.follow(i, callback=self.ajax_request)
 
-        for i in range(1,totalNumberOfPages + 1):
-            yield response.follow(self.start_url+f"&page={i}")
+        for i in range(1, totalNumberOfPages + 1):
+            yield response.follow(self.start_url + f"&page={i}")
 
-    def ajaxRequest(self,response):
-        pass
+    def ajax_request(self, response):
+        text = response.text
+        # all_texts = response.xpath('//*[(@id = "content")]//text()').getall()
+        # all_texts_string = " ".join(all_texts)
+        # all_texts_css = response.css('#content p::text').getall()
+        # test_2 = response.css("#content > div.content-body > *::text").getall()
+        # test_parsed = [" ".join(i.split()) for i in test_2 if not i.isspace()]
+        # test_parsed_string = "\n".join(test_parsed)
+        # with open('test_parsed.docx','w') as text:
+        #     text.write(test_parsed_string)
+        #
+        # test_html = response.css("#content > div.content-body").get()
+        # with open('test_html.html','w') as text:
+        #     text.write(test_html)
+        file_name = response.url.split('/')[-2]
 
-# process = CrawlerProcess()
-# process.crawl(AihrSpider)
-# process.start()
+        with open(f"/../../files/{file_name}.html",mode = 'w',encoding="utf-8") as f:
+            f.write(text)
+        # //*[contains(@class, "content-body")]//p
+        ##content > div.content-body.\33 3 > *
+        # yield {
+        #     'text' : all_texts
+        # }
+
+
+# cmdline.execute("scrapy parse --spider=aihrSpider -c ajaxRequest https://www.aihr.com/blog/pto-policy/".split())
+
+process = CrawlerProcess()
+process.crawl(AihrSpider)
+process.start()
---
+++ aihrCrawler/translate.py
@@ -0,0 +1,45 @@
+from glob import glob
+import os
+from google_trans_new import google_translator
+from scrapy import Selector
+
+translator = google_translator()
+# translate_text = translator.translate('สวัสดีจีน',lang_tgt='en')
+
+
+file = glob('files/*.html')
+
+translations_dictionary = {
+
+}
+
+all_texts = []
+
+with open(file[0], mode='r', encoding='utf-8') as f:
+    file_content = f.read()
+    file_len = len(file_content)
+    response = Selector(text=file_content)
+    response_translate = file_content
+    all_p_tags = response.css('*').getall()
+    all_p_tags_texts = response.css('*::text').getall()
+    all_texts.extend(all_p_tags_texts)
+
+    all_p_tags_translations = ["test" for i in all_p_tags_texts]
+    translations_dict = dict(zip(all_p_tags_texts, all_p_tags_translations))
+
+    for i in translations_dict:
+        response_translate = response_translate.replace(i, translations_dict[i])
+
+    # for i in all_p_tags_texts:
+    #     if i not in translations_dictionary:
+    #         translations_dictionary[i] = translations_dict[i]
+
+    # all_texts = response.css("*")
+#
+#     test_2 = response.css("#content > div.content-body > *::text").getall()
+#     test_parsed = [" ".join(i.split()) for i in test_2 if not i.isspace()]
+#     test_parsed_string = "\n".join(test_parsed)
+#     len_test = len(test_parsed_string)
+#     translate_text = translator.translate('สวัสดีจีน', lang_tgt='en')
+#
+#     translate_text = translator.translate(text[2], lang_tgt='en')
